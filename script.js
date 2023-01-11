a6dd53dc7880dc73a1c423d9a244ea2457125599
nick-Wolfgang <nicklarsonwolfgang@gmail.com>
2023-01-11 16:24:07 +0100

    fxns Definitions

---
+++ script.js
@@ -0,0 +1,31 @@
+/**
+ * TODO: Update the text in the "Formatted Text" section as a user types in the textarea
+ * TODO TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
+ * TODO: Add an .underline class to "Formatted Text" when Underline button is clicked
+ * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
+ */
+
+
+
+function updateText(){
+    // CODE GOES HERE
+  }
+  
+  function makeBold(elem){
+    //CODE GOES HERE
+  }
+  
+  /**
+   * Toggle the italic class for the output text
+   */
+  function makeItalic(elem){
+    //CODE GOES HERE
+  }
+  
+  function makeUnderline(elem){
+      //CODE GOES HERE
+  }
+  
+  function alignText(elem, alignType){
+    // CODE GOES HERE
+  }
